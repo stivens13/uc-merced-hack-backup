b90b77f1a1819c6572a8bc52de74b6aa5eaef13a
Akash Shekara <akashshekara1212@gmail.com>
2017-02-11 03:57:07 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,72 @@
+module.exports = function(io) {
+    var app = require('express');
+    var router = app.Router();
+    var fs = require('fs');
+
+    router.get('/', function(req, res, next) {
+      // while(true){
+      //   // servo.value = servo.value - 1;
+      //   // servo.to(servo.value);
+      //   console.log(servo.value);
+      // };
+      res.render('index',{title: ""});
+    });
+
+    // router.post('/',function(req, res, next) {
+    //   var response = req.body;
+    //   setSteeringAngle(response.angle);
+    // });
+
+
+    var five = require("johnny-five")
+      , board = new five.Board();
+    
+    var servo;
+    var image;
+
+    board.on("ready", function() {
+      servo = new five.Servo({
+        pin: 9
+      });
+      // console.log("hello");
+    });
+
+
+    // set an angle between 0 - 180 degrees
+    var setSteeringAngle = function(angle){
+      servo.to(angle);
+    }
+
+    var predictSteeringAngle = function(image){
+      return;
+    }
+
+    io.on('connection', function(socket) { 
+        console.log("a user connected");
+
+        // steering data sent from the browser
+        socket.on('steeringAngle', function (data) {
+          // change steering angle
+          setSteeringAngle(data);
+        }); 
+
+
+        socket.on('log', function(data){
+          fs.appendFile('driving_dataset/data.txt', data["img"] + ".jpg" + " " + data["str"] + "\n", function(err){
+            if (err) {
+
+            }else { 
+              console.log('File saved.');
+            }
+          });
+        });
+
+        socket.on('img', function(data){
+          fs.writeFile("driving_dataset/" + data["num"] + ".jpg", data["img"], 'base64', function(err){
+            // console.log(err);
+          });
+        });
+    });
+
+    return router;
+}
---
+++ run_dataset.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import scipy.misc
+import model
+import cv2
+from subprocess import call
+
+sess = tf.InteractiveSession()
+saver = tf.train.Saver()
+saver.restore(sess, "save/model.ckpt")
+
+img = cv2.imread('steering_wheel_image.jpg',0)
+rows,cols = img.shape
+smoothed_angle = 0
+i = 0
+
+def remap( x, oMin, oMax, nMin, nMax ):
+    #range check
+    if oMin == oMax:
+        print "Warning: Zero input range"
+        return None
+    if nMin == nMax:
+        print "Warning: Zero output range"
+        return None
+    #check reversed input range
+    reverseInput = False
+    oldMin = min( oMin, oMax )
+    oldMax = max( oMin, oMax )
+    if not oldMin == oMin:
+        reverseInput = True
+    #check reversed output range
+    reverseOutput = False   
+    newMin = min( nMin, nMax )
+    newMax = max( nMin, nMax )
+    if not newMin == nMin :
+        reverseOutput = True
+    portion = (x-oldMin)*(newMax-newMin)/(oldMax-oldMin)
+    if reverseInput:
+        portion = (oldMax-x)*(newMax-newMin)/(oldMax-oldMin)
+    result = portion + newMin
+    if reverseOutput:
+        result = newMax - portion
+    return result
+
+while(cv2.waitKey(250) != ord('q')):
+    full_image = scipy.misc.imread("driving_dataset/" + str(i) + ".jpg", mode="RGB")
+    image = scipy.misc.imresize(full_image[-150:], [66, 200]) / 255.0
+    degrees = model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180 / scipy.pi
+    call("clear")
+    print("Predicted steering angle: " + str(degrees) + " degrees")
+    cv2.imshow("frame", cv2.cvtColor(full_image, cv2.COLOR_RGB2BGR))
+    #make smooth angle transitions by turning the steering wheel based on the difference of the current angle
+    #and the predicted angle
+    # smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
+    converted_angle = remap(degrees, 0, 180, -90, 90)
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),-converted_angle,1)
+    dst = cv2.warpAffine(img,M,(cols,rows))
+    cv2.imshow("steering wheel", dst)
+    i += 1
+
+cv2.destroyAllWindows()
