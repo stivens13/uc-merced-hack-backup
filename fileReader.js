d04ba26ee1610497ada524d1ba5a63690b432110
jmarcelsw <jmarcelsw@gmail.com>
2017-02-11 15:27:36 -0800

    js file reader

---
+++ fileReader.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+fs.writeFile("time.txt", "fuuccckkkk", (err) => {
+    if (err) {
+        console.log(err);
+    }
+
+})
+
+fs.readFile("time.txt", {}, (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(data)
+})
